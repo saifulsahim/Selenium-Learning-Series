bf537a47eaf088fcb49f68c9bb4b1e89ce976f2f
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-09-14 22:30:14 +0600

    Chapter 15 Relative locators pasrt 1

--- src/CH15_Selenium4_latest_features/RelativeLocators.java
+++ src/CH15_Selenium4_latest_features/RelativeLocators.java
@@ -1,14 +1,23 @@
 package CH15_Selenium4_latest_features;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import static org.openqa.selenium.support.locators.RelativeLocator.*;
+
 
 public class RelativeLocators {
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Webdriver\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://formy-project.herokuapp.com/autocomplete");
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		
+		// Syntax : driver.findElement(with.TagName("XX").above(WebElement)
+		
+		WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
+		System.out.println(driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText());
 
 	}
 

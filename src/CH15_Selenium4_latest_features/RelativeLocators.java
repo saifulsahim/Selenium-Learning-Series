9693bf97ab0dbe54dd4e947ff0b84a20bc50356c
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-09-14 23:00:23 +0600

    Below, Above, toLeftOF, toRightOf locators

--- src/CH15_Selenium4_latest_features/RelativeLocators.java
+++ src/CH15_Selenium4_latest_features/RelativeLocators.java
@@ -15,9 +15,25 @@ public class RelativeLocators {
 		driver.get("https://rahulshettyacademy.com/angularpractice/");
 		
 		// Syntax : driver.findElement(with.TagName("XX").above(WebElement)
+		// Tag name is optional -> By.cssSelector("[name='name']")
 		
 		WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
 		System.out.println(driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText());
+		
+		//Below 
+		WebElement dateOfBirth = driver.findElement(By.cssSelector("[for='dateofBirth']"));
+		driver.findElement(with(By.tagName("input")).below(dateOfBirth)).click();
+		
+		//toLeftOf
+		WebElement iceCreamLabel = driver.findElement(By.xpath("//label[text()='Check me out if you Love IceCreams!']"));
+		driver.findElement(with(By.tagName("input")).toLeftOf(iceCreamLabel)).click();
+		
+		// toRightOf
+		WebElement rdb = driver.findElement(By.id("inlineRadio1")); 
+		System.out.println(driver.findElement(with(By.tagName("label")).toRightOf(rdb)).getText());
+		
+		
+
 
 	}
 

f87a3796e90435e88e2896223ad71f23e23a4900
saifu <sahimalam96@gmail.com>
2025-09-15 12:59:55 +0600

    Capture Webelement Screenshot using Selenium

---
+++ src/CH15_Selenium4_latest_features/webElementPartialScreenshot.java
@@ -0,0 +1,52 @@
+package CH15_Selenium4_latest_features;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class webElementPartialScreenshot {
+
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Webdriver\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		/*
+		 * Invoking Multiple Windows/Tabs from Selenium using one driver Instance
+		 * 
+		 * Scenario : Navigate to https://rahulshettyacademy.com/angularpractice/ Fill
+		 * the “Name” field with the first course name available at
+		 * https://rahulshettyacademy.com
+		 */
+		driver.switchTo().newWindow(WindowType.WINDOW);
+
+		Set<String> handles = driver.getWindowHandles();
+		Iterator<String> it = handles.iterator();
+		String parentWindowId = it.next();
+		String childWindowId = it.next();
+		driver.switchTo().window(childWindowId);
+		driver.get("https://rahulshettyacademy.com");
+		//Using regular expression to find out first course name always
+		String courseName = driver.findElements(By.cssSelector("a[href*='https://courses.rahulshettyacademy.com/p']"))
+				.get(1).getText();
+		driver.switchTo().window(parentWindowId);
+		WebElement name = driver.findElement(By.cssSelector("[name='name']"));
+		name.sendKeys(courseName);
+		
+		/*
+		 * Capture Webelement Screenshot using Selenium
+		 */
+
+		File file = name.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(file, new File("logo.png"));
+	}
+
+}

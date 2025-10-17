661f53b3a3cab3951c32a22cb127fd1584341e70
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-10-17 19:15:38 +0600

    Handling Window Authentication Popup

---
+++ src/CH30_FileUploading_AutoIT_And_donwloading/windowPopUp.java
@@ -0,0 +1,18 @@
+package CH30_FileUploading_AutoIT_And_donwloading;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class windowPopUp {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Webdriver\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		// Syntax : https://Username:password@SiteURL
+		driver.get("https://admin:admin@the-internet.herokuapp.com/");
+		driver.findElement(By.linkText("Basic Auth")).click();
+	}
+
+}

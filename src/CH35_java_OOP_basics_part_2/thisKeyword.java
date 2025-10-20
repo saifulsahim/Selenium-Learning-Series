43212441e8b91efe9e32ead8d1389280d8fd79a6
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-10-21 00:16:31 +0600

    this keyword

---
+++ src/CH35_java_OOP_basics_part_2/thisKeyword.java
@@ -0,0 +1,20 @@
+package CH35_java_OOP_basics_part_2;
+
+public class thisKeyword {
+		int a = 2;
+
+		public void getData()
+		{
+		    int a = 3;
+		    System.out.println(a);
+		    System.out.println(this.a);
+		    // this refers to current object - object scope lies in class level
+		}
+
+		public static void main(String[] args) {
+		    // TODO Auto-generated method stub
+			thisKeyword td = new thisKeyword();
+		    td.getData();
+		}
+
+}

e88daedb348ab9c8c8d12a55cee663b43b6aacb0
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-10-21 00:28:32 +0600

    final keyword

---
+++ src/CH35_java_OOP_basics_part_2/finalKeyword.java
@@ -0,0 +1,26 @@
+package CH35_java_OOP_basics_part_2;
+
+//this, super, try, catch, finally
+
+final class finaldemo {
+  /*final void getData(){
+      
+  }*/
+
+  // class name as final..that means you cannot extend that class
+
+  public static void main(String[] args) {
+      // TODO Auto-generated method stub
+
+      final int i = 4; // constant variables
+
+      // i = 5;
+  }
+}
+
+
+
+
+
+
+

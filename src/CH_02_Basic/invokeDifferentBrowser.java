8edd81760faa9589b56b9b765c1efdb37acfd106
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-10-22 23:32:20 +0600

    invking different browser

--- src/CH_02_Basic/invokeDifferentBrowser.java
+++ src/CH_02_Basic/invokeDifferentBrowser.java
@@ -1,14 +1,19 @@
-package Basic;
+package CH_02_Basic;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Testing {
+public class invokeDifferentBrowser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Webdriver\\chromedriver-win64\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
+		//WebDriver driver = new EdgeDriver();
+		
 		driver.get("https://www.automationexercise.com");
 
 	}

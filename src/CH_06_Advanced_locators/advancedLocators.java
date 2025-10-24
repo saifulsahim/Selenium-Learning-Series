55198ace77edd002743462c796368b274b7baee4
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-10-24 21:33:28 +0600

    Parse the String with Java methods to get the password dynamically from the page

--- src/CH_06_Advanced_locators/advancedLocators.java
+++ src/CH_06_Advanced_locators/advancedLocators.java
@@ -14,18 +14,36 @@ public class advancedLocators {
 		WebDriver driver = new ChromeDriver();
 		
 		String name = "Miqdad";
+		String password= getPassword(driver);
 		driver.get("https://rahulshettyacademy.com/locatorspractice/");
  		driver.findElement(By.id("inputUsername")).sendKeys(name);
-		driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
+ 		
+		driver.findElement(By.name("inputPassword")).sendKeys(password);
 		driver.findElement(By.className("signInBtn")).click(); 
 		
-		Thread.sleep(1000); // waiting to load page after login
+		Thread.sleep(2000); // waiting to load page after login
 		//System.out.println(driver.findElement(By.tagName("p")).getText());
 		Assert.assertEquals(driver.findElement(By.tagName("p")).getText(), "You are successfully logged in.");
 		
 		Assert.assertEquals(driver.findElement(By.cssSelector("div[class='login-container'] h2")).getText(), "Hello "+ name +",");
 		driver.findElement(By.xpath("//button[text()='Log Out']")).click();
 		//driver.close();
+	}
+	
+	public static String getPassword(WebDriver driver) throws InterruptedException
+	{
+		driver.get("https://rahulshettyacademy.com/locatorspractice/");
+		driver.findElement(By.linkText("Forgot your password?")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.cssSelector(".reset-pwd-btn")).click();
+		String passwordText = driver.findElement(By.cssSelector("form p")).getText(); 
+		// Please use temporary password 'rahulshettyacademy' to Login.
+		// Oth index - Please use temporary password '
+		// 1st index- rahulshettyacademy' to Login.
+		String[] passwordArray = passwordText.split("'");
+		String password = passwordArray[1].split("'")[0];
+		return password;
+
 	}
 	
 	/*

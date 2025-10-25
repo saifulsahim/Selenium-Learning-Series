f8168dbaf9feb47ce18be058b004a7ac547950f3
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-10-25 15:02:13 +0600

    Dynamic dropdown

---
+++ src/CH_07_Web_Elements/DynamicDropDown.java
@@ -0,0 +1,28 @@
+package CH_07_Web_Elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropDown {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Webdriver\\chromedriver-win64\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		 //a[@value='MAA'] - Xpath for chennai
+		 //a[@value='BLR'] - Bengaluru
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("(//a[@value='DEL'])[2]")).click(); // Index 2 is used because Selenium counts options from the top (starting at 0), so “Delhi” is the 2nd item in the dropdown "To".
+		
+
+
+
+
+	}
+
+}

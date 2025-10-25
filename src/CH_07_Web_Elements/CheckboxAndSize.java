d2c5f68145dffcdfccb64907288825b6800d7c90
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-10-25 19:08:03 +0600

    Handling checkbox & get their size

---
+++ src/CH_07_Web_Elements/CheckboxAndSize.java
@@ -0,0 +1,40 @@
+package CH_07_Web_Elements;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckboxAndSize {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Webdriver\\chromedriver-win64\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		
+		driver.findElement(By.id("autosuggest")).sendKeys("ban");
+		Thread.sleep(3000);
+		List<WebElement> options= driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+		for(WebElement option : options) {
+			if(option.getText().equalsIgnoreCase("Bangladesh"))
+			{
+				option.click();
+				break;
+			}
+		}
+		
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click(); //using regular expression clicking checkbox
+		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+
+		//COUNT no of checkbox
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		
+	}
+
+}

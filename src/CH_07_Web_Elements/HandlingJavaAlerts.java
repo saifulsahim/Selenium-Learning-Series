0633eda1841823a781f4cac602493412cf80ee2c
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-10-25 23:59:15 +0600

    Handle alerts

---
+++ src/CH_07_Web_Elements/HandlingJavaAlerts.java
@@ -0,0 +1,26 @@
+package CH_07_Web_Elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandlingJavaAlerts {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Webdriver\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.manage().window().maximize();
+		
+		String text = "Miqdad";
+		driver.findElement(By.id("name")).sendKeys(text);
+		driver.findElement(By.cssSelector("[id='alertbtn']")).click(); // Tagname is optional in Css 
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		driver.findElement(By.id("confirmbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+ 
+	}
+
+}

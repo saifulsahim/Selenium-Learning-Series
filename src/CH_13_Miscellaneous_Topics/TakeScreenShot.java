8bde6a06de683366512e99f6cd0b4bcbbb20ea5a
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-11-01 11:58:58 +0600

    Taking screenshot

---
+++ src/CH_13_Miscellaneous_Topics/TakeScreenShot.java
@@ -0,0 +1,26 @@
+package CH_13_Miscellaneous_Topics;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TakeScreenShot {
+
+	public static void main(String[] args) throws IOException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Webdriver\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("F:\\Selenium\\Selenium_Learning_Series\\screenshot.png"));
+		
+		
+	} 
+
+}

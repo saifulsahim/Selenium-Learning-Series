dda03c7fbf31cb4f2efa3535251ef4eb9b8030e3
Saiful Alam Sahim <sahimalam96@gmail.com>
2025-11-01 11:21:08 +0600

    Setting proxy port & download directory

--- src/CH_13_Miscellaneous_Topics/HttpCertficate.java
+++ src/CH_13_Miscellaneous_Topics/HttpCertficate.java
@@ -1,5 +1,9 @@
 package CH_13_Miscellaneous_Topics;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,6 +12,19 @@ public class HttpCertficate {
 
 	public static void main(String[] args) {
 		ChromeOptions options = new ChromeOptions();
+		/*
+		Proxy proxy = new Proxy();
+		proxy.setHttpProxy("ipaddess:4444");
+		options.setCapability("proxy", proxy);
+		*/
+		
+		/* Set download directory
+		 * 
+		Map<String, Object> prefs = new HashMap<String, Object>();
+		prefs.put("download.default_directory", "/directory/path");
+		options.setExperimentalOption("prefs", prefs);
+		*/
+		
 		options.setAcceptInsecureCerts(true); // accept insecure certificate
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Webdriver\\chromedriver-win64\\chromedriver.exe");
